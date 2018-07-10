b68be91428637a9c9100e2ef3b66a4a910837964
Mateo Balcorta <misterrattail@gmail.com>
2018-07-10 15:48:38 -0700

    working navigation bar.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import "../../../css/materialize.css";
+import Dropdown from "../Dropdown/Dropdown.js";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+// import "../../../css/materialize.css";
+// import "../../../materialize_js/materialize.js";
+import "./Navigation.css"
 
 class Navigation extends Component  {
     constructor(props){
@@ -7,26 +11,41 @@ class Navigation extends Component  {
     }
 
     render() {
-
         return (
             <div>
-                <nav class="white" role="navigation">
-                    <div className="nav-wrapper container">
-                    <a id="logo-container" href="#home" className="brand-logo">Progressive Talent Pipeline</a>
-                        <ul className="right hide-on-med-and-down">
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="#about">About the Program</a></li>
-                            <li><a href="#work">Why Work on Capitol Hill</a></li>
-                            <li><a href="#apply">Why Apply for the Pipeline?</a></li>
-                            <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">More<i className="material-icons right">arrow_drop_down</i></a></li>
-                            <li><a href="sass.html"><i className="material-icons left">search</i></a></li> 
-                        </ul>
-                    </div>
-                </nav>
+                <Navbar collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                        <a href="#brand">Progressive Talent Pipeline</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav> 
+                        </Nav>
+                        <Nav pullRight>
+                        <NavItem eventKey={1} href="#home">
+                            Home
+                        </NavItem>
+                        <NavItem eventKey={2} href="#about">
+                            About the Program
+                        </NavItem>
+                        <NavItem eventKey={3} href="#capitol">
+                            Why Work on Capitol Hill
+                        </NavItem>
+                        <NavItem eventKey={4} href="#pipeline">
+                            Why Apply for the Pipeline?
+                        </NavItem>
+                        <NavDropdown eventKey={5} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={5.1} href="#who">Who we are looking for</MenuItem>
+                            <MenuItem eventKey={5.2} href="#apply">Apply</MenuItem>
+                        </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>    
             </div>
         )
     }
 }
-
 
 export default Navigation;
