eab201fc26c97fd819d58a2ab612f114776fc5b8
Mateo Balcorta <misterrattail@gmail.com>
2018-07-10 16:14:08 -0700

    Added text content.

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -22,6 +22,15 @@ class Content extends Component {
                 <div>
                     <Headers headerText="Progressive Talent Pipeline"/>
                 </div>
+                <div>
+                    <p>
+                    A “blue wave” this November could bring transformative change to Washington. But, to realize the potential, it’s not only necessary to win elections. It’s also necessary to plan for what happens after a Democratic takeover in one or both houses on Capitol Hill.
+                    </p>
+                    <p>
+                    A “blue wave” this November could bring transformative change to Washington. But, to realize the potential, it’s not only necessary to win elections. It’s also necessary to plan for what happens after a Democratic takeover in one or both houses on Capitol Hill.
+                    </p>
+
+                </div>
             </div>
           );
     }
