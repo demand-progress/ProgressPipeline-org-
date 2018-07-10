c6fe31be5d1531972c09878dd41401a0cd94128f
Mateo Balcorta <misterrattail@gmail.com>
2018-07-10 15:48:13 -0700

    Remove navigation from app to content component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import { hot } from "react-hot-loader";
-import Message from "./Components/Message/Message.js";
-import "normalize.css";
-import "./App.css";
-import Navigation from "./Components/Navigation/Navigation.js"
+import React, { Component } from "react"
+import { hot } from "react-hot-loader"
+import Message from "./Components/Message/Message.js"
+import "normalize.css"
+import "./App.css"
+import Content from "./Components/Content/Content.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="main-header">
-         <Navigation />
-        </header>
-        <section className="container">
+         
+          <section className="container">
+          <Content />
           <Message message="Welcome to the react-starter. Start hacking away!" />
         </section>
       </div>
