7860d07e7e2e31097aa2991ea6393cf24f985854
Mateo Balcorta <misterrattail@gmail.com>
2018-07-10 10:33:26 -0700

    Moved materialize js files to folder.

---
+++ materialize_js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+    $(".dropdown-trigger").dropdown();
+    $("#dropdown1").dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
