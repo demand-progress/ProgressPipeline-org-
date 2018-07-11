9715401ee78b8952d4dcd685e9ba095ca950460e
Mateo Balcorta <misterrattail@gmail.com>
2018-07-10 22:34:51 -0700

    Added Apply page.

---
+++ src/Components/Content/Apply.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+
+const Apply = () => {
+    return(
+        <div>
+            <h2>Apply</h2>
+            <div>
+                <p>Applying for the talent pipeline is simple and straightforward.  Upload two documents below, following the instructions.</p>
+                <ol>
+                    <li>Your CV or Resume</li>
+                    <li>A document with your answers to the following two questions:
+                        <ol>
+                            <li>Why do you want to work on Capitol Hill? (300 Words)</li>
+                            <li>Name three pieces of legislation for which you would want to advocate and why.  (Up to 300 words)</li>
+                        </ol>
+                    </li>
+                </ol>
+            </div>
+            <div>
+                <p>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSdFliu2CAVouMrOf0fTuzlJSnwyG5jfU7ceBdLguWabFDdXdQ/viewform?oldembedui=true">Open link to Apply</a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Apply 
