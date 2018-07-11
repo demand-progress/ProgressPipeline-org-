d1e6c9e4ac74a66f72de57f0b7e51add288c15b2
Mateo Balcorta <misterrattail@gmail.com>
2018-07-10 22:37:12 -0700

    Added onclick events to anchor tags.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -9,11 +9,14 @@ class Navigation extends Component  {
         super(props)
     }
 
-    render() {
-        return (
+ 
 
+    render() {
+        const { navigatePage } = this.props
+
+        return (
             <div id="navHeader">
-                <Navbar fixedTop collapseOnSelect>
+                <Navbar fixedTop collapseOnSelect >
                     <Navbar.Header>
                         <Navbar.Brand>
                         <a href="#brand">Progressive Talent Pipeline</a>
@@ -24,21 +27,21 @@ class Navigation extends Component  {
                         <Nav> 
                         </Nav>
                         <Nav pullRight>
-                        <NavItem eventKey={1} href="#home">
+                        <NavItem eventKey={1} href="#home" onClick={ navigatePage }>
                             Home
                         </NavItem>
-                        <NavItem eventKey={2} href="#about">
+                        <NavItem eventKey={2} href="#about" onClick={ navigatePage }>
                             About the Program
                         </NavItem>
-                        <NavItem eventKey={3} href="#capitol">
+                        <NavItem eventKey={3} href="#capitol" onClick={ navigatePage }>
                             Why Work on Capitol Hill
                         </NavItem>
-                        <NavItem eventKey={4} href="#pipeline">
+                        <NavItem eventKey={4} href="#pipeline" onClick={ navigatePage }>
                             Why Apply for the Pipeline?
                         </NavItem>
-                        <NavDropdown eventKey={5} title="Dropdown" id="basic-nav-dropdown">
-                            <MenuItem eventKey={5.1} href="#who">Who we are looking for</MenuItem>
-                            <MenuItem eventKey={5.2} href="#apply">Apply</MenuItem>
+                        <NavDropdown eventKey={5} title="More" id="basic-nav-dropdown">
+                            <MenuItem eventKey={5.1} href="#who" onClick={ navigatePage }>Who we are looking for</MenuItem>
+                            <MenuItem eventKey={5.2} href="#apply" onClick={ navigatePage }>Apply</MenuItem>
                         </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
