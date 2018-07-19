b7e141f2ec0e2c68557e6a16cfb3344ff1aee068
Mateo Balcorta <misterrattail@gmail.com>
2018-07-19 10:50:59 -0700

    Update Nav to only have one onSelect function.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
-// import "../../../css/materialize.css";
-// import "../../../materialize_js/materialize.js";
 import "./Navigation.css"
 
 class Navigation extends Component  {
     constructor(props){
         super(props)
     }
-
- 
 
     render() {
         const { navigatePage } = this.props
@@ -26,22 +22,22 @@ class Navigation extends Component  {
                     <Navbar.Collapse>
                         <Nav> 
                         </Nav>
-                        <Nav pullRight>
-                        <NavItem eventKey={1} href="#home" onClick={ navigatePage }>
+                        <Nav pullRight onSelect={navigatePage}>
+                        <NavItem eventKey={1} href="#home">
                             Home
                         </NavItem>
-                        <NavItem eventKey={2} href="#about" onClick={ navigatePage }>
+                        <NavItem eventKey={2} href="#about">
                             About the Program
                         </NavItem>
-                        <NavItem eventKey={3} href="#capitol" onClick={ navigatePage }>
+                        <NavItem eventKey={3} href="#capitol">
                             Why Work on Capitol Hill
                         </NavItem>
-                        <NavItem eventKey={4} href="#pipeline" onClick={ navigatePage }>
+                        <NavItem eventKey={4} href="#pipeline">
                             Why Apply for the Pipeline?
                         </NavItem>
                         <NavDropdown eventKey={5} title="More" id="basic-nav-dropdown">
-                            <MenuItem eventKey={5.1} href="#who" onClick={ navigatePage }>Who we are looking for</MenuItem>
-                            <MenuItem eventKey={5.2} href="#apply" onClick={ navigatePage }>Apply</MenuItem>
+                            <MenuItem eventKey={5.1} href="#who">Who we are looking for</MenuItem>
+                            <MenuItem eventKey={5.2} href="#apply">Apply</MenuItem>
                         </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
