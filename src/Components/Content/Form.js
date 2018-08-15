c9c973c394590ad84ae8da59d0daae87ddb0b988
Mateo Balcorta <misterrattail@gmail.com>
2018-08-15 15:59:46 -0700

    Remove height and width inline varibles.

--- src/Components/Content/Form.js
+++ src/Components/Content/Form.js
@@ -22,7 +22,7 @@ class Form extends Component  {
         return(
             <div>
                 <div className="loading" style={{display: this.state.pageLoaded ? 'none': 'block' }}>Loading Form ...</div> 
-                <iframe style={{display: this.state.pageLoaded ? 'block': 'none'}} src="https://script.google.com/macros/s/AKfycbyBk0pVaexA-mycr5xX1zZM4P7cVu-FblAfhEc_KwMNqkOVWA/exec" width="100%" height="1000px" frameBorder="0" scrolling="no"></iframe> 
+                <iframe style={{display: this.state.pageLoaded ? 'block': 'none'}} src="https://script.google.com/macros/s/AKfycbyBk0pVaexA-mycr5xX1zZM4P7cVu-FblAfhEc_KwMNqkOVWA/exec" frameBorder="0" scrolling="no"></iframe> 
                 <div className="footer">
                     <p>For any questions please contact <a href="mailto:progressivepipelineproject@gmail.com">progressivepipelineproject@gmail.com</a></p>
                 </div>
