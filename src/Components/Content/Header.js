33cbb7a5a411c2cb2158394d1b0b5574e2c6ad21
Mateo Balcorta <misterrattail@gmail.com>
2018-08-16 08:46:48 -0700

    Added className to apply header.

--- src/Components/Content/Header.js
+++ src/Components/Content/Header.js
@@ -2,10 +2,25 @@ import React from "react"
 
 
 const Header = (props) => {
-  return (
-      <div className="header">
+  let head = (
+    <div className="header">
+      <p> {props.headerText} </p>
+    </div>
+    )
+
+  if(props.addClass){
+    const classWithProp = `header ${props.addClass}`
+    head = (
+      <div className={classWithProp}>
         <p> {props.headerText} </p>
       </div>
+    )
+  }
+
+  return (
+    <div>
+      {head}
+    </div>
   )
 }
 
