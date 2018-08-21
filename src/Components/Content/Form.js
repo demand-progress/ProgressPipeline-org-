c0faad3bd94ab5a824d6d799df705fa53609763b
Mateo Balcorta <misterrattail@gmail.com>
2018-08-21 14:31:22 -0700

    Remove page footer from form component.

--- src/Components/Content/Form.js
+++ src/Components/Content/Form.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 
 class Form extends Component  {
     constructor(props){
-        super(props)
+        super(props);
         this.state = {
             pageLoaded: false
-        }
+        };
     }
 
     componentDidMount(){
         setTimeout(
             function() {
-                this.setState({pageLoaded: true})
+                this.setState({pageLoaded: true});
             }
             .bind(this),
             1500
@@ -22,13 +22,10 @@ class Form extends Component  {
         return(
             <div>
                 <div className="loading" style={{display: this.state.pageLoaded ? 'none': 'block' }}>Loading Form ...</div> 
-                <iframe style={{display: this.state.pageLoaded ? 'block': 'none'}} src="https://script.google.com/macros/s/AKfycbxj-3SLm569HNfuBotbdwCpBwFIrf5E4iD6JttCk7YWAVeQV58m/exec" frameBorder="0" scrolling="no"></iframe> 
-                <div className="footer">
-                    <p>For any questions please contact <a href="mailto:progressivepipelineproject@gmail.com">progressivepipelineproject@gmail.com</a></p>
-                </div>
+                <iframe style={{display: this.state.pageLoaded ? 'block': 'none'}} src="https://script.google.com/macros/s/AKfycbxj-3SLm569HNfuBotbdwCpBwFIrf5E4iD6JttCk7YWAVeQV58m/exec" frameBorder="0" scrolling="no"></iframe>
             </div> 
-        )
+        );
     }
 }
 
-export default Form
+export default Form;
