f2dd69be1d4698333feb65e5d3f2b858f886a67c
Oliver Wise <owise1@gmail.com>
2020-08-06 16:38:02 -0700

    twitter embed

--- src/Components/Content/Jobs.js
+++ src/Components/Content/Jobs.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Jobs.scss"
 import classNames from 'classnames'
 import { getPage } from "../../utils";
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 export default class Jobs extends Component  {
   state = {}
@@ -19,7 +20,10 @@ export default class Jobs extends Component  {
       <div className="container">
         <div className="content" dangerouslySetInnerHTML={{ __html: page.content.rendered }} />
         <div className="twitter">
-          <a className="twitter-timeline" href="https://twitter.com/opengovjobs?ref_src=twsrc%5Etfw">Tweets by opengovjobs</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="opengovjobs"
+          />
         </div>
       </div>
     </div>
